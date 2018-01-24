b94ff75369d2f56078b630f91b620fff12110b28
Sergey Ushakov <sergeyushakov430@gmail.com>
2018-01-24 16:16:15 +0200

    New dental partner

--- dentists/dentists.js
+++ dentists/dentists.js
@@ -1,5 +1,6 @@
 document.getElementById("form-modal-1").style.display = 'none';
 document.getElementById("formTwo").style.display = 'none';
+document.getElementById("formThree").style.display = 'none';
 
 function formModalOne() {
     document.getElementById("modal").style.display = 'none';
@@ -10,4 +11,12 @@ function formModalTwo() {
     document.getElementById("modal").style.display = 'none';
     document.getElementById("form-modal-1").style.display = 'block';
     document.getElementById("formTwo").style.display = 'block';
+}
+
+function formModalThree() {
+    document.getElementById("modal").style.display = 'none';
+    document.getElementById("form-modal-1").style.display = 'block';
+    document.getElementById("formThree").style.display = 'block';
+    document.getElementById("practice").innerHTML = "Company name:";
+    document.getElementById("rooms-cell").style.display = 'none';
 }
