784369180e8f7cb9e06292074ab50435a1d9e062
Jeremias <grenzebach@pm.me>
2018-07-31 14:03:43 +0300

    test

--- wallet/uPort.js
+++ wallet/uPort.js
@@ -1,27 +1,32 @@
+
 #!/usr/bin/ nodejs
 
-import { Connect, SimpleSigner } from 'uport-connect'
+const uportConnect = require('uport-connect');
+const qrcode = require('qrcode-terminal');
 
-    const uport = new Connect('Dentacoin Wallet', {
-      clientId: '2oqwHbLQ8Ao28LvFFThbd1Jxpj45EBPb8jd',
-      network: 'rinkeby',
-      signer: SimpleSigner('33e090f34dbd2fc20e87408f90c078d1018bd77fb8806bb0b69da945fb4eaefc'),
-    })
+const mnidAddress = '2oqwHbLQ8Ao28LvFFThbd1Jxpj45EBPb8jd';
+const signingKey = require('./uPortKey');
+const appName = 'Dentacoin Wallet';
 
-    // Request credentials to login
-    uport.requestCredentials({
-      requested: ['name', 'phone', 'country'],
-      notifications: true // We want this if we want to recieve credentials
-    })
-    .then((credentials) => {
-      // Do something
-    })
+const uriHandler = (uri) => {
+  qrcode.generate(uri, {small: true})
+  console.log(uri)
+}
 
-    // Attest specific credentials
-    uport.attestCredentials({
-      sub: THE_RECEIVING_UPORT_ADDRESS,
-      claim: {
-        CREDENTIAL_NAME: CREDENTIAL_VALUE
-      },
-      exp: new Date().getTime() + 30 * 24 * 60 * 60 * 1000, // 30 days from now
-    })
+const uport = new uportConnect.Connect(appName, {
+    uriHandler,
+    clientId: mnidAddress,
+    network: 'rinkeby',
+    signer: uportConnect.SimpleSigner(signingKey.key)
+});
+
+const web3 = uport.getWeb3();
+console.log(web3.eth.getCoinbase());
+
+
+// Request credentials
+uport.requestCredentials({
+  requested: ['name'],
+}).then((credentials) => {
+  console.log(credentials);
+})
