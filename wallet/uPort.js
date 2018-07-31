83295c547dcfcd59e5885a7126bd26eda73e5115
Jeremias <grenzebach@pm.me>
2018-07-31 11:49:49 +0300

    test

--- wallet/uPort.js
+++ wallet/uPort.js
@@ -1,17 +1,17 @@
 const uportConnect = require('uport-connect');
-const qrcode = require('qrcode-terminal');
+//const qrcode = require('qrcode-terminal');
 
 const mnidAddress = '2oqwHbLQ8Ao28LvFFThbd1Jxpj45EBPb8jd';
 const signingKey = require('./uPortKey');
 const appName = 'Dentacoin-Wallet';
-
+/*
 const uriHandler = (uri) => {
   qrcode.generate(uri, {small: true})
   console.log(uri)
 }
-
+*/
 const uport = new uportConnect.Connect(appName, {
-    uriHandler,
+    //uriHandler,
     clientId: mnidAddress,
     network: 'rinkeby',
     signer: uportConnect.SimpleSigner(signingKey.key)
