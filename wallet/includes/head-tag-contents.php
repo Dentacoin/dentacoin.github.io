192be863f673261855b3dde02ca5b759f1b5eaca
danielkolarov <37291474+danielkolarov@users.noreply.github.com>
2018-08-31 15:55:52 +0300

    wallet

--- wallet/includes/head-tag-contents.php
+++ wallet/includes/head-tag-contents.php
@@ -2,7 +2,7 @@
 <meta charset="utf-8">
     <title>Dentacoin Wallet</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
